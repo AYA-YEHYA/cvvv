5730458daca6e8d476a9134705acd8f8bfb674ae
AYA-YEHYA <zat162002@gmail.com>
2024-04-01 21:13:34 +0200

    .

--- index.py
+++ index.py
@@ -1,20 +1,14 @@
 import cv2
-import numpy as np
-from scipy import ndimage
-kernel_3x3 = np.array([[-1, -1, -1],
-[-1, 8, -1],
-[-1, -1, -1]])
-
-img=cv2.imread("Figure1")
-k3=ndimage.convlove(img,kernel_3x3)
- 
-blured=cv2.GaussianBlur(img,(7,7),0)
-g_hbf=img-blured
-
-cv2.imshow('originalImage',img)
-cv2.imshow('3x3',k3)
-cv2.imshow('blureImage',blured)
-cv2.imshow('g_hbf',g_hbf)
-
-cv2.waitKey()
+import numpy as np 
+pipes=cv2.imread('img1.jpg')
+gray_img = cv2.cvtColor(pipes,cv2.COLOR_BGR2GRAY)
+gray_img = cv2.medianBlur(gray_img , 5)
+cricles = cv2.HoughCircles(gray_img,cv2.HOUGH_GRADIENT,1,120,param1=100,param2=30,minDist=0,minRadius=0)
+cricles =np.uint16(np.around(cricles))
+for i in cricles[0,:]:
+    cv2.circle(pipes,(i[0],i[1],2,(0,255,0),2))
+    cv2.circle(pipes,(i[0],i[1],2,(0,0,255),3))
+cv2.imwrite('img1.jpg',pipes)   
+cv2.imshow("HoughCricles",pipes)
+cv2.waitKey()   
 cv2.destroyAllWindows()
